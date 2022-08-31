76314325adb2278bd01809f2062920c2324f3a44
razscalelot <you@example.com>
2022-08-31 15:41:22 +0530

    req file version changes

--- FestumEvento-festumevento_server-d9c4a2e91c3f/festumevento/settings.py
+++ FestumEvento-festumevento_server-d9c4a2e91c3f/festumevento/settings.py
@@ -252,6 +252,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
